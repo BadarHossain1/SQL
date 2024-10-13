ebadfdf6f749c14800065f0c2541b6801e2ab885
Badar Hossain <badar12041@gmail.com>
2024-10-13 10:59:03 +0600

    Subqueries

--- ForeignKey.sql
+++ ForeignKey.sql
@@ -24,6 +24,7 @@ create table temp(
 	cust_id int,
     foreign key (cust_id) references temp1(id)
     on delete cascade -- Deletes the row from temp1 table if the row is deleted from temp table.
+    on delete set null -- Sets the column value null but also check this to verify Badar!
     on update cascade -- Updates the row from temp1 table if the row is updated from temp table. 
     
 );
